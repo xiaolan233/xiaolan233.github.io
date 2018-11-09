eb1bed0111a39e289a4e7ca0d017c555b03dd6d3
lanlan2017 <18251956727@163.com>
2018-11-10 01:11:28 +0800

    Site updated: 2018-11-10 01:11:21

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Fri Nov 09 2018 23:32:38 GMT+0800 (中国标准时间)
+// build time:Sat Nov 10 2018 01:11:17 GMT+0800 (中国标准时间)
 $(document).ready(function(){var t=$(".sidebar-inner");function e(){return $(".header-inner").height()+CONFIG.sidebar.offset}function i(){var t=$(".footer-inner");var e=t.outerHeight(true)-t.outerHeight();var i=t.outerHeight(true)+e;return i}function r(t){return $("#sidebar").css({"margin-top":t})}function n(){var n=e();var a=i();var f=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight();var o=$("#content").height();if(n+f<o){t.affix({offset:{top:n-CONFIG.sidebar.offset,bottom:a}})}r(n).css({"margin-left":"initial"})}function a(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");n()}function f(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){a()}})}n();f()});
 //rebuild by neat 
