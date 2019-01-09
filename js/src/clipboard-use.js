3aa0df7a231fa2b1f70226b8a19c5fec442cc3b0
lanlan2017 <18251956727@163.com>
2019-01-09 18:32:24 +0800

    Site updated: 2019-01-09 18:32:17

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 08 2019 18:49:31 GMT+0800 (中国标准时间)
+// build time:Wed Jan 09 2019 18:32:12 GMT+0800 (中国标准时间)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
