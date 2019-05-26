e8beb55f8882cc16985ccfe6c4de14bfea4903d2
lanlan2017 <18251956727@163.com>
2019-05-26 22:55:00 +0800

    Site updated: 2019-05-26 22:54:55

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Fri May 24 2019 14:55:55 GMT+0800 (中国标准时间)
+// build time:Sun May 26 2019 22:54:49 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]===o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
